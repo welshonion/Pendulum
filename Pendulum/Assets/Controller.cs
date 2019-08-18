10ac0a99338c68b8d1dc61b1a8e2875fb1616504
welshonion <45817850+welshonion@users.noreply.github.com>
2019-08-18 20:07:15 +0900

    Checked weight

---
+++ Pendulum/Assets/Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainCamera;
+    [SerializeField]
+    private GameObject subCamera;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.SetActive(true);
+        subCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Startbutton()
+    {
+        SceneManager.LoadScene("Scene");
+    }
+
+    public void Endbutton()
+    {
+        Application.Quit();
+    }
+
+    public void SwitchCamera()
+    {
+        mainCamera.SetActive(!mainCamera.activeSelf);
+        subCamera.SetActive(!subCamera.activeSelf);
+    }
+}
